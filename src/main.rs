0b807fd294f01c84336463fd1d35e3bae31201bb
Timo Wolf <timo.wolf@gmx.ch>
2025-10-29 16:02:16 +0100

    crude saving logic implemented

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use chrono::{Datelike, Local};
 use eframe::egui;
+use std::fs;
 
 fn main() -> eframe::Result<()> {
     let options = eframe::NativeOptions::default();
@@ -83,12 +84,53 @@ impl Note {
 // -----------------------------
 struct NoteApp {
     note: Note,
+    save_path: Option<String>,
+    last_save_msg: Option<String>,
+}
+
+impl NoteApp {
+    fn ui(&mut self, ui: &mut egui::Ui) {
+        self.note.ui(ui);
+        ui.separator();
+
+        ui.text_edit_singleline(self.save_path.get_or_insert_with(|| format!("note{}_{}_{}.txt", self.note.date.year, self.note.date.month, self.note.date.day)));
+
+        if ui.button("Save Note").clicked() {
+            self.save();
+            self.last_save_msg = Some("Note saved successfully!".into());
+        }
+
+        if let Some(msg) = &self.last_save_msg {
+            ui.colored_label(egui::Color32::GREEN, msg);
+        }
+    }
+
+    fn save(&self) {
+        if let Some(path) = &self.save_path {
+            let content = format!(
+                "Title: {}\nDate: {:02}-{:02}-{}\n\n{}",
+                self.note.title,
+                self.note.date.day,
+                self.note.date.month,
+                self.note.date.year,
+                self.note.content
+            );
+
+            if let Err(err) = fs::write(path, content) {
+                eprintln!("Error saving note: {}", err);
+            } else {
+                println!("✅ Saved note to {}", path);
+            }
+        }
+    }
 }
 
 impl Default for NoteApp {
     fn default() -> Self {
         Self {
             note: Note::new("New Note".into(), "".into(), Date::default()),
+            save_path: None,
+            last_save_msg: None,
         }
     }
 }
@@ -96,7 +138,7 @@ impl Default for NoteApp {
 impl eframe::App for NoteApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            self.note.ui(ui);
+            self.ui(ui);
         });
     }
 }
